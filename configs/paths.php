2341d801477c885a93263c89b72d01c64a8003ae
Zezzman <francoisleroux97@gmail.com>
2020-05-08 02:21:27 +1000

    layout css changes

--- configs/paths.php
+++ configs/paths.php
@@ -10,18 +10,5 @@ return [
         'VENDOR' => 'vendor/',
         'STORAGE' => 'storage/',
         'RESOURCES' => ['resources/'],
-    ],
-    'CLOSURES' =>
-    [
-        'RESOURCE' => function (string $name)
-        {
-            $root = requireConfig('PATHS.ROOT');
-            $resources = (array) requireConfig('PATHS.RESOURCES');
-            foreach ($resources as $resource)
-            {
-                if (file_exists($path = $root . $resource .  $name))
-                    return $path;
-            }
-        }
     ]
 ];
